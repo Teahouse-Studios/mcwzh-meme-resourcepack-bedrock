10234aee00bf15d6cab262065845f66e41aad350
SkyEye_FAST <820843448@qq.com>
2020-03-03 18:25:18 +0800

    Update textures

--- build.py
+++ build.py
@@ -31,11 +31,8 @@ def build(args):
             pack.write("meme_resourcepack/texts/" + file)
     # build with textures
     if not args['without_texture']:
-        for file in os.listdir("meme_resourcepack/textures/environment"):
-            pack.write("meme_resourcepack/textures/environment/" + file)
         for file in os.listdir("meme_resourcepack/textures/ui"):
             pack.write("meme_resourcepack/textures/ui/" + file)
-        pack.write("meme_resourcepack/textures/painting/kz.png")
         pack.write("meme_resourcepack/textures/map/map_background.png")
     pack.close()
 
