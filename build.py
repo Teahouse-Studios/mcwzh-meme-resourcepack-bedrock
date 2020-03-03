cf7fb1ffc49a915f5bd446c9372ccd1a32dfa93a
lakejason0 <sunliyuan200402@outlook.com>
2020-03-03 23:16:55 +0800

    Hotfix

--- build.py
+++ build.py
@@ -34,10 +34,10 @@ def build(args):
         for file in os.listdir("meme_resourcepack/textures/ui"):
             pack.write("meme_resourcepack/textures/ui/" + file)
         for file in os.listdir("meme_resorcepack/textures/entity"):
-            pack.write("meme_resorcepack/textures/entity" + file)
+            pack.write("meme_resourcepack/textures/entity" + file)
         for file in os.listdir("meme_resorcepack/textures/items"):
-            pack.write("meme_resorcepack/textures/items" + file)
-        pack.write("meme_resorcepack/models/mobs.json)
+            pack.write("meme_resourcepack/textures/items" + file)
+        pack.write("meme_resourcepack/models/mobs.json")
         pack.write("meme_resourcepack/textures/map/map_background.png")
     pack.close()
 
