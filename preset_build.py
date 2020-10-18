e6425d26e0ebcd8373fee29931d79a1230eaeb07
MysticNebula70 <34984380+AlPha5130@users.noreply.github.com>
2020-10-18 18:21:19 +0800

    remove some zip presets

--- preset_build.py
+++ preset_build.py
@@ -4,12 +4,8 @@ if __name__ == '__main__':
     from os.path import exists, isdir, join
 
     preset_args = [
-        {'type': 'zip', 'compatible': False, 'modules': {
+        {'type': 'zip', 'compatible': True, 'modules': {
             'resource': []}, 'hash': False, 'output': 'builds'},
-        {'type': 'zip', 'compatible': False, 'modules': {
-            'resource': ['blue_ui']}, 'hash': False, 'output': 'builds'},
-        {'type': 'zip', 'compatible': False, 'modules': {'resource': [
-            'a_letter', 'bagify', 'observer_think', 'trident_model']}, 'hash': False, 'output': 'builds'},
         {'type': 'mcpack', 'compatible': False, 'modules': {
             'resource': []}, 'hash': False, 'output': 'builds'},
         {'type': 'mcpack', 'compatible': False, 'modules': {
@@ -18,22 +14,17 @@ if __name__ == '__main__':
             'a_letter', 'bagify', 'observer_think', 'trident_model']}, 'hash': False, 'output': 'builds'},
         {'type': 'mcpack', 'compatible': True, 'modules': {
             'resource': []}, 'hash': False, 'output': 'builds'},
-        {'type': 'zip', 'compatible': True, 'modules': {
-            'resource': []}, 'hash': False, 'output': 'builds'},
         {'type': 'zip', 'compatible': False, 'modules': {
             'resource': ['all']}, 'hash': False, 'output': 'builds'},
         {'type': 'mcpack', 'compatible': False, 'modules': {
             'resource': ['all']}, 'hash': False, 'output': 'builds'}
     ]
     preset_name = [
-        "meme-resourcepack_noresource_noblueui.zip",
-        "meme-resourcepack_noresource.zip",
-        "meme-resourcepack_noblueui.zip",
+        "meme-resourcepack_compatible_noresource_noblueui.zip",
         "meme-resourcepack_noresource_noblueui.mcpack",
         "meme-resourcepack_noresource.mcpack",
         "meme-resourcepack_noblueui.mcpack",
-        "meme-resourcepack_noresource_noblueui_compat.mcpack",
-        "meme-resourcepack_noresource_noblueui_compat.zip",
+        "meme-resourcepack_compatible_noresource_noblueui.mcpack",
         "meme-resourcepack.zip",
         "meme-resourcepack.mcpack"
     ]
