8d11e4c5c7959e3fc81ed19e816532d5b025a4f9
MysticNebula70 <34984380+AlPha5130@users.noreply.github.com>
2020-10-18 16:14:52 +0800

    update

--- build.py
+++ build.py
@@ -1,24 +1,27 @@
 from os.path import join, dirname
 
-if __name__ == 'meme-pack-bedrock.build':
+if __name__ == f'{dirname(__file__)}.build':
     from .packaging.pack_builder import pack_builder
     from .packaging.module_checker import module_checker
 else:
     from packaging.pack_builder import pack_builder
     from packaging.module_checker import module_checker
 
-# License: Apache-2.0
+
+def check_module(module_path=None):
+    module_path = module_path or join(dirname(__file__), "modules")
+    checker = module_checker(module_path)
+    checker.check_module()
+    return checker.module_info, checker.check_info_list
 
 
 def build(args: dict):
     build_info = []
     current_dir = dirname(__file__)
-    # init module_checker
-    checker = module_checker(join(current_dir, "modules"))
-    # checking module integrity
-    checker.check_module()
-    build_info.extend(checker.info_list)
-    builder = pack_builder(join(current_dir, "meme_resourcepack"), checker.module_info)
+    module_info, module_check_info = check_module()
+    build_info.extend(module_check_info)
+    builder = pack_builder(
+        join(current_dir, "meme_resourcepack"), module_info)
     builder.args = args
     builder.build()
     build_info.extend(builder.log_list)
