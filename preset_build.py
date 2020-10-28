34ee83ae09bc36e182608797a95bae064d177b70
MysticNebula70 <34984380+AlPha5130@users.noreply.github.com>
2020-10-28 14:40:16 +0800

    add version check

--- preset_build.py
+++ preset_build.py
@@ -1,53 +1,73 @@
 if __name__ == '__main__':
     import build
+    from json import load
     from os import listdir, mkdir, remove, rename
     from os.path import exists, isdir, join
+    from sys import exit
 
-    preset_args = [
-        {'type': 'zip', 'compatible': True, 'modules': {
-            'resource': []}, 'hash': False, 'output': 'builds'},
-        {'type': 'mcpack', 'compatible': False, 'modules': {
-            'resource': []}, 'hash': False, 'output': 'builds'},
-        {'type': 'mcpack', 'compatible': False, 'modules': {
-            'resource': ['blue_ui']}, 'hash': False, 'output': 'builds'},
-        {'type': 'mcpack', 'compatible': False, 'modules': {'resource': [
-            'a_letter', 'bagify', 'observer_think', 'trident_model']}, 'hash': False, 'output': 'builds'},
-        {'type': 'mcpack', 'compatible': True, 'modules': {
-            'resource': []}, 'hash': False, 'output': 'builds'},
-        {'type': 'zip', 'compatible': False, 'modules': {
-            'resource': ['all']}, 'hash': False, 'output': 'builds'},
-        {'type': 'mcpack', 'compatible': False, 'modules': {
-            'resource': ['all']}, 'hash': False, 'output': 'builds'}
-    ]
-    preset_name = [
-        "meme-resourcepack_compatible_noresource_noblueui.zip",
-        "meme-resourcepack_noresource_noblueui.mcpack",
-        "meme-resourcepack_noresource.mcpack",
-        "meme-resourcepack_noblueui.mcpack",
-        "meme-resourcepack_compatible_noresource_noblueui.mcpack",
-        "meme-resourcepack.zip",
-        "meme-resourcepack.mcpack"
-    ]
-    pack_counter = 0
-    perfect_pack_counter = 0
-    base_folder = "builds"
-    if exists(base_folder) and not isdir(base_folder):
-        remove(base_folder)
-    if not exists(base_folder):
-        mkdir(base_folder)
-    for file in listdir(base_folder):
-        remove(join(base_folder, file))
-    for args, name in zip(preset_args, preset_name):
-        pack_name, warning_count, error, _ = build.build(args)
-        if not error:
-            pack_counter += 1
-            if warning_count == 0:
-                perfect_pack_counter += 1
-            if name != "meme-resourcepack.zip" and name != "meme-resourcepack.mcpack":
-                rename(join(base_folder, pack_name),
-                       join(base_folder, name))
-                print(f"Renamed pack to {name}.")
-        else:
-            print(f"Failed to build pack {name}.")
-    print(
-        f"\nBuilt {pack_counter} packs with {perfect_pack_counter} pack(s) no warning.")
+    pack_version = '1.0.1'
+    build_unsuccessful = 0
+
+    def check_version_consistency():
+        manifest = load(
+            open("meme_resourcepack/manifest.json", 'r', encoding='utf8'))
+        header_version = '.'.join(
+            map(lambda i: str(i), manifest['header']['version']))
+        modules_version = '.'.join(
+            map(lambda i: str(i), manifest['modules'][0]['version']))
+        return pack_version == header_version and pack_version == modules_version
+
+    if check_version_consistency():
+        preset_args = [
+            {'type': 'mcpack', 'compatible': False, 'modules': {
+                'resource': ['all']}, 'hash': False, 'output': 'builds'},
+            {'type': 'mcpack', 'compatible': False, 'modules': {
+                'resource': ['blue_ui']}, 'hash': False, 'output': 'builds'},
+            {'type': 'mcpack', 'compatible': False, 'modules': {'resource': [
+                'a_letter', 'bagify', 'observer_think', 'trident_model']}, 'hash': False, 'output': 'builds'},
+            {'type': 'mcpack', 'compatible': False, 'modules': {
+                'resource': []}, 'hash': False, 'output': 'builds'},
+            {'type': 'mcpack', 'compatible': True, 'modules': {
+                'resource': []}, 'hash': False, 'output': 'builds'},
+            {'type': 'zip', 'compatible': False, 'modules': {
+                'resource': ['all']}, 'hash': False, 'output': 'builds'},
+            {'type': 'zip', 'compatible': True, 'modules': {
+                'resource': []}, 'hash': False, 'output': 'builds'},
+        ]
+        preset_name = [
+            f"meme-resourcepack_v{pack_version}.mcpack",
+            f"meme-resourcepack_noresource_v{pack_version}.mcpack",
+            f"meme-resourcepack_noblueui_v{pack_version}.mcpack",
+            f"meme-resourcepack_noresource_noblueui_v{pack_version}.mcpack",
+            f"meme-resourcepack_compatible_noresource_noblueui_v{pack_version}.mcpack",
+            f"meme-resourcepack_v{pack_version}.zip",
+            f"meme-resourcepack_compatible_noresource_noblueui_v{pack_version}.zip",
+        ]
+        pack_counter = 0
+        perfect_pack_counter = 0
+        base_folder = "builds"
+        if exists(base_folder) and not isdir(base_folder):
+            remove(base_folder)
+        if not exists(base_folder):
+            mkdir(base_folder)
+        for file in listdir(base_folder):
+            remove(join(base_folder, file))
+        for args, name in zip(preset_args, preset_name):
+            pack_name, warning_count, error, _ = build.build(args)
+            if not error:
+                pack_counter += 1
+                if warning_count == 0:
+                    perfect_pack_counter += 1
+                if name != "meme-resourcepack.zip" and name != "meme-resourcepack.mcpack":
+                    rename(join(base_folder, pack_name),
+                           join(base_folder, name))
+                    print(f"Renamed pack to {name}.")
+            else:
+                print(f"Failed to build pack {name}.")
+                build_unsuccessful = 1
+        print(
+            f"\nBuilt {pack_counter} packs with {perfect_pack_counter} pack(s) no warning.")
+        exit(build_unsuccessful)
+    else:
+        exit(
+            f'\033[1;31mError: Pack version "{pack_version}" does not match number in manifest.json.\033[0m')
